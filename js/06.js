8ac597f21820574b131bb63e86fc308d57cba0e7
sergiocruzC0122 <thiorfthix@gmail.com>
2024-11-08 09:36:59 -0500

    Aprendiendo numeros , operadores y Math

---
+++ js/06.js
@@ -0,0 +1,22 @@
+//Objeto math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.2); //Redondea el numero
+resultado = Math.ceil(2.1); //Redondea al mayor
+resultado = Math.floor(2.9); //Redondea al menor
+resultado = Math.sqrt(144); //Le saca la raiz al numero
+resultado = Math.abs(-200); //Lo convierte a positivo
+resultado = Math.min( 3, 2, 10, 8, 9, 15); //Devuelve el numero menor
+resultado = Math.max( 3, 2, 10, 8, 9, 15); //Devuelve el numero meyor
+resultado = Math.random();//genera un numero ramdom
+resultado = Math.floor(Math.random() * 30); //Redondea al menor el numero generado por random y multiplicado por 30
+
+
+
+
+
+
+
+console.log(resultado);
