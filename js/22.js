d6ee9fa9734e21b6fdbc99b3544e65809c213f79
sergiocruzC0122 <thiorfthix@gmail.com>
2024-11-12 07:35:28 -0500

    Uso del if

---
+++ js/22.js
@@ -0,0 +1,29 @@
+
+// const puntaje = 1000;
+
+// if(puntaje == 1000){
+//     console.log('El puntaje es 1000');
+// } else {
+//     console.log('No es igual');
+// }
+
+
+// const efectivo = 1000;
+// const carrito = 800;
+
+// if(efectivo > carrito){
+//     console.log('El usuario puede pagar');
+// } else {
+//     console.log('El usuario No puede pagar');
+// }
+
+
+const rol = 'Administrador';
+
+if( rol == "Administrador"){
+    console.log('Eres adminitrador puede editar mucho');
+} else if ( rol == "Editor"){
+    console.log('Eres editor, por lo tanto no puedes editar mucho');
+} else {
+    console.log('no tienes acceso')
+}
